26aedca0446f1fcd4a107c43b1cf1a91f24eb3e0
Edward Rush <edwardrush@Edwards-MacBook-Pro.local>
2018-09-15 20:35:06 -0400

    Initial Commit

---
+++ madt/Views/cpcc/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace madt.Views.cpcc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
