e36fe86962381cd6a8ec9dc00ea4804ddb520066
Edward Rush <edwardrush@Edwards-MacBook-Pro.local>
2018-09-16 13:49:14 -0400

    ReAdded base grid template

---
+++ madt/Views/cpcc/GridMainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace madt.Views.cpcc
+{
+    public partial class GridMainPage : ContentPage
+    {
+        public GridMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
